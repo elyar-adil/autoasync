d2b5e96d69f1335e319787e40b3cdb6f3854a7cc
Elyar Adil <Elyar.Adil@gmail.com>
2026-04-25 20:20:15 +0800

    Release v0.1.1

--- autoasync/__init__.py
+++ autoasync/__init__.py
@@ -2,5 +2,5 @@ from autoasync._proxy import LazyProxy
 from autoasync._core import autoasync, configure_autoasync, reset_autoasync, run_sync
 
 __all__ = ["autoasync", "configure_autoasync", "reset_autoasync", "run_sync", "LazyProxy"]
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "elyar-adil"
